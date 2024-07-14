150afb53189191f79b3c29341f79ab3918ed5056
Luke Clark <lukeclarksfo@gmail.com>
2024-07-14 19:02:14 +0200

    Update

--- tensorflow/core/activity_watcher/activity_utils.cc
+++ tensorflow/core/activity_watcher/activity_utils.cc
@@ -15,8 +15,22 @@ namespace activity_watcher {
 std::unique_ptr<Activity> ActivityFromContext(
     OpKernelContext* context, tsl::string name, ActivityCategory category,
     Activity::Attributes additional_attributes) {
-        
+    Activity::Atrributes attributes(std::move(additional_attributes));
+    
+    if(context)
+    {
+        attributes.merge(Activity::Attributes({
+            {"node_name", context->op_kernel().def().name()},
+            {"step_id", absl::StrCat(context->step_id()},
+            {"device", context>device()->name()},
+            {"op", context->op_kernel().def().op()},
+            {"iter_num", absl::StrCat(context->frame_iter().iter_id)},
+            {"inputs", absl::StrJoin(context->op_kernel().def().input(), ";")},
+        }));
     }
 
+    return std::make_unique<Activity>(name, category, std::move(attributes));
 }
-}
+
+} // namespace activity_watcher
+} // namespace tensorflow
